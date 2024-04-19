262e3391a2af6db2f06fb746c6cec84fe375e8e6
Thanos Antonopoulos <thanos@kalamaki.home>
2024-04-19 15:19:29 +0300

    dwm-thanos 6.5

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#3B4252";
 static const char col_gray3[]       = "#D8DEE9";
 static const char col_gray4[]       = "#ECEFF4";
-static const char col_cyan[]        = "#796878";
+static const char col_cyan[]        = "#434C5E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
