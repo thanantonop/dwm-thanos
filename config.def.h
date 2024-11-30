0c4a72ee0d9efd6e61006953e73a8b38d173f963
Thanos Antonopoulos <thanos@tinos.home>
2024-11-30 23:21:39 +0200

    dwm-thanos 6.5

--- config.def.h
+++ config.def.h
@@ -13,14 +13,14 @@ static const int showsystray             = 1;        /* 0 means no systray */
 static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
 static const Bool viewontag              = True;     /* Switch view on tag switch */
-static const char *fonts[]               = { "MesloLGS Nerd Font Mono:size=10" };
-static const char dmenufont[]            = "MesloLGS Nerd Font Mono:size=10";
-static const char normbordercolor[]      = "#3B4252";
-static const char normbgcolor[]          = "#2E3440";
-static const char normfgcolor[]          = "#D8DEE9";
-static const char selbordercolor[]       = "#EBCB8B";
-static const char selbgcolor[]           = "#8FBCBB";
-static const char selfgcolor[]           = "#2E3440";
+static const char *fonts[]               = { "Carlito:size=12" };
+static const char dmenufont[]            = "Carlito:size=12";
+static const char normbordercolor[]      = "#004275";
+static const char normbgcolor[]          = "#004275";
+static const char normfgcolor[]          = "#FFFFFF";
+static const char selbordercolor[]       = "#FF7200";
+static const char selbgcolor[]           = "#005A9E";
+static const char selfgcolor[]           = "#FFFFFF";
 
 static const char *colors[][3] = {
     /*               fg           bg           border   */
@@ -97,7 +97,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-font", "MesloLGS Nerd Font Mono 10", NULL };
+static const char *roficmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-font", "Carlito 12", NULL };
 static const char *termcmd[] = { "st", "-f", "MesloLGS Nerd Font Mono:size=10", NULL };
 static const char *termcmd_kitty[]  = { "kitty", NULL };
 static const char *termcmd_alacritty[]  = { "alacritty", NULL };
